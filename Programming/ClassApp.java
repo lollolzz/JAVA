c7dcf3db4c859ca5ac04871a69b6f677b75311c9
lollolzz <lollolzz1018@gmail.com>
2022-05-10 23:27:09 +0900

    java~

---
+++ Programming/ClassApp.java
@@ -0,0 +1,15 @@
+import java.io.FileNotFoundException;
+public class ClassApp {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(1.6));
+		System.out.println(Math.ceil(1.6));
+		
+		// class는 연관된 변수와 메소드를 모아서 이름을 붙인 것이다.
+		
+
+	}
+
+}
