3cd0aefb562ff8dd5500da9c697a2c7a140b9227
lollolzz <lollolzz1018@gmail.com>
2022-05-11 22:54:56 +0900

    java!!

--- Programming/InstanceApp.java
+++ Programming/InstanceApp.java
@@ -6,14 +6,14 @@ public class InstanceApp {
 
 	public static void main(String[] args) throws IOException {
 
-//		PrintWriter p1 = new PrintWriter("result1.txt");
-//		// new를 붙여 복제한 결과를 p1에 담았다, p1 앞의 PrintWriter는 p1을 규제하기 위한 것 
-//		p1.write("Hello 1");
-//		p1.close();
-//		
-//		PrintWriter p2 = new PrintWriter("result2.txt");
-//		p2.write("Hello 2");
-//		p2.close();
+		PrintWriter p1 = new PrintWriter("result1.txt");
+		// new를 붙여 복제한 결과를 p1에 담았다, p1 앞의 PrintWriter는 p1을 규제하기 위한 것 
+		p1.write("Hello 1");
+		p1.close();
+		
+		PrintWriter p2 = new PrintWriter("result2.txt");
+		p2.write("Hello 2");
+		p2.close();
 		
 		// instance를 사용하지 않을 경우 
 		// 변수에다가 저장해 두었다 사용하는 것이 더 편리하다고 알면 됨 
