6fecd746193e92e28f57314a28e0c309feceaf4d
lollolzz <lollolzz1018@gmail.com>
2022-05-23 23:08:29 +0900

    java 복습

---
+++ java-flow-control/src/AuthApp.java
@@ -0,0 +1,45 @@
+
+public class AuthApp {
+
+	public static void main(String[] args) {
+		
+		
+		String id = "egoing";
+		String inputid = args[0];
+		
+		String pass = "1111";
+		String inputPass = args[1];
+		
+		System.out.println("Hi");
+		
+//		if(inputid == id) {
+		
+//		if(inputid.equals(id)) {
+//			if(inputPass.equals(pass)) {
+//				System.out.println("Master!");
+//			}else {
+//				System.out.println("Wrong password!");
+//			}
+//		} else {
+//			
+//			System.out.println("Who are you?");
+//			
+//		}
+		
+		if(inputid.equals(id) && inputPass.equals(pass)) {
+
+			System.out.println("Master!");
+			
+		} else {
+			
+			System.out.println("Who are you?");
+			
+		}
+		
+		
+		
+		 
+
+	}
+
+}
---
+++ java-flow-control/src/BooleanApp.java
@@ -0,0 +1,23 @@
+
+public class BooleanApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("One");
+		System.out.println(1);
+		
+		System.out.println(true);
+		System.out.println(false);
+		
+		String foo = "Hello world";
+		// String true = "Hello World";
+		// "" 가 없는 true는 boolean이라는 약속이 되어있다.
+		// reserved word
+		
+		System.out.println(foo.contains("world"));
+		System.out.println(foo.contains("egoing"));
+		
+
+	}
+
+}
